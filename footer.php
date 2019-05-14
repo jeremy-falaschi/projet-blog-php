c0774a28b645c7c1935c95c592f6c09dbb0b27a5
jeremy <jeremy.falaschi@gmail.com>
2019-05-14 15:55:24 +0200

    ajout de la base des fichier du blog

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 bloc_footer">
+            <h2>le blog de Jean Forteroche</h2>
+            <p>Retrouver tous les chapitres de mon nouveau roman, un billet simple pour l'alaska</p>
+        </div>
+        <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 bloc_footer">
+            <h2>Contact</h2>
+            <p>Email : <a href="mailto:jeanforteroche@gmail.com">jeanforteroche@gmail.com</a><br/>Site officiel : <a href="https://jeanforteroche.com">www.jeanforteroche.com</a></p>
+        </div>
+        </div>
+    </div>
+</footer>
